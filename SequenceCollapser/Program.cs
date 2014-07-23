e7c61b1fefe04fb3597c6e15973b9b9a055f4ae6
sramisetty <sramisetty@gmail.com>
2014-07-23 11:05:12 -0400

    Version 0.1

---
+++ SequenceCollapser/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SequenceCollapser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            var number=int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= number; i++)
+            {
+                int j = i;
+                int pItem = -1;
+                List<int> items = new List<int>();
+                while (j > 0)
+                {
+                    int item = (int)(j % 10);
+                    if (pItem != item)
+                    {
+                        items.Insert(0, item);
+                        pItem = item;
+                    }
+                    j = j / 10;
+                }
+                Console.WriteLine(i + " : " + string.Join("", items));
+            }
+           
+        }
+    }
+}
